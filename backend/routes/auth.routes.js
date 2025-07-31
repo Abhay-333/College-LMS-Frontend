da0ef98c3bdaa2a8065a061cb997d0a31b4db30a
Abhay-333 <abhayd0331@gmail.com>
2025-07-31 21:38:53 +0530

    update

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -11,9 +11,9 @@ import { checkAuth } from "../middlewares/checkAuth.js";
 const authRouter = express(Router());
 
 authRouter.get("/", Home);
-authRouter.post("/signup", signUp);
-authRouter.post("/login", login);
-authRouter.post("/logout", logout);
-authRouter.get("/getUser", checkAuth, getUserData);
+authRouter.post("/api/signup", signUp);
+authRouter.post("/api/login", login);
+authRouter.post("/api/logout", logout);
+authRouter.get("/api/getUser", checkAuth, getUserData);
 
 export default authRouter;
--- frontend/src/Utils/UserContext.jsx
+++ frontend/src/Utils/UserContext.jsx
@@ -10,7 +10,7 @@ function UserContext({ children }) {
   const [userData, setUserData] = useState(null);
   const getUserData = async () => {
     try {
-      let { data } = await axios.get(serverUrl + "/getUser", {
+      let { data } = await axios.get(serverUrl + "/api/getUser", {
         withCredentials: true,
       });
       console.log(data);
