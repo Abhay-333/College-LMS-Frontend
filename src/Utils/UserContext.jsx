827e32a99c3851485ce3cdc299c405321799058f
Abhay-333 <abhayd0331@gmail.com>
2025-08-01 16:23:58 +0530

    done

--- src/Utils/UserContext.jsx
+++ src/Utils/UserContext.jsx
@@ -6,7 +6,7 @@ export const dataContext = createContext();
 
 function UserContext({ children }) {
   const navigation = useNavigate();
-  const serverUrl = "https://college-lms.vercel.app/api/";
+  const serverUrl = "https://your-backend-url.onrender.com/api/";
   const [userData, setUserData] = useState(null);
   const getUserData = async () => {
     try {
